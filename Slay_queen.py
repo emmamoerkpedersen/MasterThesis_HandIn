1d733b46e3633a3ba71c20b7cf208c630d3349bf
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2025-01-13 11:45:10 +0100

    ryd op

--- Slay_queen.py
+++ Slay_queen.py
@@ -1,8 +1,1 @@
 import numpy as np
-#Emma det her lort skal virke!!!! 
-#Det er din skyld det ikke virker
-#FUUUUUUUUCK GITHUUUUUB;
-#få det til at virke
-#DILLLER!!!!
-#få det til at virke
-#diller 2
