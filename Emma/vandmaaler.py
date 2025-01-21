c76fca6ec5b67f4d8149b1b1957c010c57163ef1
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2025-01-21 16:23:02 +0100

    Emma changes

--- Emma/vandmaaler.py
+++ Emma/vandmaaler.py
@@ -3,5 +3,9 @@ import numpy as np
 
 data = pd.read_csv('/Users/emmamork/Downloads/Vandkemi - Hydrometri_20250121_145426.csv', sep =';')
 df = pd.DataFrame(data)
+df['x-koordinat'] = df['x-koordinat'].str.replace(',', '.')
+df['y-koordinat'] = df['y-koordinat'].str.replace(',', '.')
 
-df.to_excel("output.xlsx")
+
+df.to_excel("output.xlsx", index=False)
+df.to_csv("output.csv", index=False)
