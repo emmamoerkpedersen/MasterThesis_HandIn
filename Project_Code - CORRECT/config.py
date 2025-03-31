56b158a091bdb3ae581ec6410734980011977327
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2025-03-31 12:54:37 +0200

    For hpc

--- Project_Code - CORRECT/config.py
+++ Project_Code - CORRECT/config.py
@@ -118,12 +118,12 @@ LSTM_CONFIG = {
     'model_type': 'seq2seq_forecaster',
     'feature_cols': ['temperature', 'rainfall'],
     'output_features': ['vst_raw'],
-    'hidden_size': 164,        
-    'num_layers': 2,          
+    'hidden_size': 124,        
+    'num_layers': 5,          
     'dropout': 0.25,          
-    'batch_size': 1,          
-    'learning_rate': 0.00001,   
-    'epochs': 20,             
+    'batch_size': 5,          
+    'learning_rate': 0.001,   
+    'epochs': 100,             
     'patience': 8, 
     
 } 
