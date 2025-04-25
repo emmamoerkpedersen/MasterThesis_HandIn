222490b81cc498e80921d9d1242605a0b8783c22
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2025-04-25 13:03:14 +0200

    preprocessing back

--- Project_Code - CORRECT/_3_lstm_model/preprocessing_LSTM.py
+++ Project_Code - CORRECT/_3_lstm_model/preprocessing_LSTM.py
@@ -131,9 +131,9 @@ class DataPreprocessor:
         # train_data = data
 
         # Split data based on years
-        test_data = data[(data.index.year == 2013)]
-        val_data = data[(data.index.year >= 2011) & (data.index.year <= 2012)]  # Validation is 2022-2023
-        train_data = data[data.index.year < 2011]  # Training is everything before 2022
+        test_data = data[(data.index.year == 2024)]
+        val_data = data[(data.index.year >= 2022) & (data.index.year <= 2023)]  # Validation is 2022-2023
+        train_data = data[data.index.year < 2022]  # Training is everything before 2022
         
         print(f"\nSplit Summary:")
         print(f"Training period: {train_data.index.min().year} - {train_data.index.max().year}")
@@ -181,7 +181,7 @@ class DataPreprocessor:
             scaled_features, scaled_target = self.feature_scaler.transform(features, target)
             
         # Create sequences
-        X, y = self._create_overlap_sequences(scaled_features, scaled_target)
+        X, y = self._create_sequences(scaled_features, scaled_target)
 
         # # Debug print for NaN values in sequences
         # print("\nChecking for NaN values in sequences:")
