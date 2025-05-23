1de0f8dd7b25164b5f67e8a26f0ac6cd96b428ab
emmamoerkpedersen <s194463@student.dtu.dk>
2025-05-23 11:08:46 +0200

    ssh stuff

--- Project_Code - CORRECT/job_submit.sh
+++ Project_Code - CORRECT/job_submit.sh
@@ -7,19 +7,19 @@
 ### Ask for number of cores (default 1)
 #BSUB -n 1
 ### Specify that we want the job to get killed if it exceeds 3 GB memory per core/slot
-#BSUB -M 5GB
+#BSUB -M 20GB
 ### Set walltime limit: hh:mm
-#BSUB -W 00:30
+#BSUB -W 02:30
 ### user email adress
 #BSUB s194463@student.dtu.dk
 ### Send notification at completion
 #BSUB -N
 ### specify the output and error file. %J is the job ID
-#BSUB -o "Output.%J.out"
-#BSUB -e "Error.%J.err"
+#BSUB -o "Output_exp7.%J.out"
+#BSUB -e "Error_exp7.%J.err"
 
 pwd
 module load python3/3.12.9
 
-python3 "main.py"
+python3 "main.py" --run_experiments
 
